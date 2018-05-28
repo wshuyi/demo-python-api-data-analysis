d818f91c7f56b350bb0829d43e7f20ff12a62a7f
Shuyi Wang <nkwshuyi@gmail.com>
2018-05-28 16:07:21 +0800

    handle chinese display properly for win32 platform

--- handle_matplotlib_chinese.py
+++ handle_matplotlib_chinese.py
@@ -1,32 +1,20 @@
 import matplotlib
 import re
 import os
+import sys
+import shutil
 
+# get config file path
 
 config_file = matplotlib.matplotlib_fname()
 
-# Make some change in the config file for matplotlib
-
-with open(config_file) as f:
-    data = f.read()
-
-regex = r"#axes\.unicode_minus"
-subst = "axes.unicode_minus"
-data = re.sub(regex, subst, data, 0)
-
-
-regex = r"#(font\.sans-serif\s+:\s+)(\S)"
-subst = "\\1WenQuanYi Micro Hei, \\2"
-data = re.sub(regex, subst, data, 0)
-
-with open(config_file, 'w') as f:
-    f.write(data)
-
 # move wenquanyi open source TTF font file in 
 
-import shutil
 
-ttf_font_path = config_file.replace('/matplotlibrc', '/fonts/ttf')
+if sys.platform == 'win32':
+    ttf_font_path = config_file.replace('\\matplotlibrc', '\\fonts\\ttf')
+else:
+    ttf_font_path = config_file.replace('/matplotlibrc', '/fonts/ttf')
 
 shutil.copy2("wenquanyi-micro-hei.ttf", ttf_font_path)
 
